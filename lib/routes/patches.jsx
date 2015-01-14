b09802bf4f22152098059a6cf5592923f9d2fa8c
George Snelling <georgesn@gmail.com>
2015-01-14 14:20:59 -0800

    layed down route map

--- lib/routes/patches.jsx
+++ lib/routes/patches.jsx
@@ -15,18 +15,17 @@ var Rows = React.createClass({
   render: function() {
 
     var rows = this.props.data.map(function(ent) {
-      log({ent: ent})
+      // log({ent: ent})
       var type = ""
       var detailsHref = "/" + ent.collection + "/" + ent._id
       var pictureUrl = utils.pictureUrl(ent.photo)
       if (ent.category && ent.category.name) type = ent.category.name
       return (
-        <div className="row">
-          <div className="col-md-4" key={ent._id}>
+        <div className="row" key={ent._id}>
+          <div className="col-md-4">
             <a href={detailsHref}><img src={pictureUrl} className="picture" /></a>
           </div>
           <div className="col-md-8">
-            {ent.schema}{": "}
             <a href={detailsHref}>{ent.name}</a>
           </div>
         </div>
